fb06c82f893d7095c7bc22e3d4109d7b1bfefd14
HallwayTech <hallwaytechgrupo@gmail.com>
2024-12-04 10:22:32 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import io from "socket.io-client";
 import PositionBoard from "./components/PositionBoard";
 import AttackBoard from "./components/AttackBoard";
 
-const socket = io("https://lobster-app-5i3ni.ondigitalocean.app");
+const socket = io("http://localhost:3001");
 
 function App() {
 	const [playerId, setPlayerId] = useState<number | null>(null);
